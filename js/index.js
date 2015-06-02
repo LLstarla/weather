a5fa3ecf418af1b3ad535b0a302bc62110e6dbc1
LLstarla <loraleestarla@me.com>
2015-06-02 02:47:53 -0700

    v3
    success happening!!

--- js/index.js
+++ js/index.js
@@ -1,53 +1,77 @@
-$(document).ready(function(){
-    
-    // Click Button
-    $('button').click(function(){
-    
-        // Store Input
-        var zip = $('input').val();
-        console.log(zip);
-
-        // GET WEATHER
-        $.simpleWeather({
-            zipcode: zip,
-            success: function(weather){
-
-    Get Condition Code
-    var currentCode = weather.code;
-    console.log(currentCode);
-    
-    if ( currentCode >= 30 && currentCode <= 36 ) {
-          
-      $('[data-role="page"]').addClass('sunny');
-      
-    }
+// Get The Weather ---------- HOME
+$.simpleWeather({
+  location: '77586', 
+  unit: 'f', 
+  success: function( weather ) {
     
     // Homescreen
     $('[data-role="content"] .temp').text( weather.temp + weather.units.temp );
     $('[data-role="content"] .city').text( weather.city + ' , ' + weather.region );
-    
-   //Check first console.log(weather.image);
-    
     $('.weather-image').attr('src', weather.image);
-    
-    //Check first console.log(weather);
-    
-  }, 
+    }, 
   
   error: function(error) {
       console.log(error);
   }
   
-});// end weather
-        
-});//end click
+});
+
+// Get The Weather ------- CHENEY
+$.simpleWeather({
+  location: '99004', 
+  unit: 'f', 
+  success: function( weather ) {
     
-    // Clear Input on Click
-    $('input:text').click(function(){
-        $('input').val('');
-    });// end clear input    
+    // Homescreen
+    $('[data-role="content"] .temp1').text( weather.temp + weather.units.temp );
+    $('[data-role="content"] .city1').text( weather.city + ' , ' + weather.region );
+    $('.weather-image1').attr('src', weather.image);
+    }, 
+  
+  error: function(error) {
+      console.log(error);
+  }
+  
+});
+
+// Get The Weather ------- SPOKANE
+$.simpleWeather({
+  location: '99201', 
+  unit: 'f', 
+  success: function( weather ) {
     
-});// end document
+    // Homescreen
+    $('[data-role="content"] .temp2').text( weather.temp + weather.units.temp );
+    $('[data-role="content"] .city2').text( weather.city + ' , ' + weather.region );
+    $('.weather-image2').attr('src', weather.image);
+    }, 
+  
+  error: function(error) {
+      console.log(error);
+  }
+  
+});
+
+// Get The Weather ------- SEATTLE
+$.simpleWeather({
+  location: '98102', 
+  unit: 'f', 
+  success: function( weather ) {
+    
+    // Homescreen
+    $('[data-role="content"] .temp3').text( weather.temp + weather.units.temp );
+    $('[data-role="content"] .city3').text( weather.city + ' , ' + weather.region );
+    $('.weather-image3').attr('src', weather.image);
+    }, 
+  
+  error: function(error) {
+      console.log(error);
+  }
+  
+});
+
+
+
 
 
 
